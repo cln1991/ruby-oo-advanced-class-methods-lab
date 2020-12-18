8cf2fa458dbff41f7ca08e9fafe93c53fa50d45e
Christopher <christophernavac@gmail.com>
2020-12-18 12:45:10 -0500

    commit tree 85ebb2ffc979c8e7d3a8925ec14e89ff272e857e

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,64 @@ class Song
     @@all
   end
 
+  def self.create
+    song = self.new
+    song.save
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+  end
+
+
+  def self.find_by_name(name)
+    @@all.find do |song|
+      name == song.name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create_by_name(name)
+  end
+
+  def self.alphabetical
+    sorted = self.all.sort_by do |song|
+      song.name
+    end
+    sorted
+  end
+
   def save
     self.class.all << self
+    self
+  end
+
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
